f28965da16b0ef8961231efe019ff8152fede59b
SamirYate1126 <131462250+SamirYate1126@users.noreply.github.com>
2025-03-03 00:06:33 -0500

    Add files via upload

---
+++ FormularySessionEight/FormularySessionEight/src/Run.java
@@ -0,0 +1,7 @@
+import co.edu.uptc.view.FormularySwing;
+
+public class Run {
+    public static void main(String[] args) throws Exception {
+        FormularySwing formularySwingObject = new FormularySwing();
+    }
+}
---
+++ FormularySessionEight/FormularySessionEight/src/co/edu/uptc/view/FormularySwing.java
@@ -0,0 +1,18 @@
+package co.edu.uptc.view;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class FormularySwing extends JFrame {
+    
+    public FormularySwing(){
+
+        super("Formulario");
+            setSize(820,640);
+            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            setLocationRelativeTo(null);
+            setVisible(true);
+        
+        
+    }
+}
